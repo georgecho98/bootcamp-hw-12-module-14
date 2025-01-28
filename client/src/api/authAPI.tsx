f2cc75bca9933073cdd1e5f0c1026707dbe4b650
Hon Chau <chaugeorge9@gmail.com>
2025-01-27 23:02:24 -0600

    starter code

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,7 +2,28 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
-}
+  try {
+    const response = await fetch(
+      '/api/works',{
+        method: 'POST',
+        headers: {
+          'Content-Type':'application/json',
+
+        },
+        body: JSON.stringify(userInfo)
+      }
+    )
+    const data = response.json();
+    if(!response.ok) {
+      throw new Error ('invalid API response, check networtk tab!');
+    }
+    return data;
+  } catch (err) {
+  console.log('Error from work Creation',err);
+  return Promise.reject('Could not create new work');
+
+  }
+};
 
 
 
